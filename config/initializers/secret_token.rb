6a70d51ed56bce16f88d18ed2e7ef293b5bfec32
Bryan <bknouse@u.rochester.edu>
2013-11-04 13:48:41 -0500

    first commit for tobacco house

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tobacco::Application.config.secret_key_base = '71a026055f3fce2fd6a69605c738802a36d5844745be7474abcf609e2ddbd39e31461a87bcd7169b5cb9c241e19318266cf456dbf596f2d173e424a6b676fb91'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tobacco::Application.config.session_store :cookie_store, key: '_tobacco_session'
