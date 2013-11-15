c67c501f871983c87a8e3efdfa42e1fe84329d34
Bryan <bknouse@u.rochester.edu>
2013-11-15 14:32:11 -0500

    updated with pictures and products page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Tobacco::Application.routes.draw do
   get "welcome/index"
   get "welcome/about"
   get "welcome/contact"
+  get "welcome/products"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
